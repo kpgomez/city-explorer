abe43db152cd5c259a088bb3db4e0cc975a14292
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-05-17 19:52:56 -0500

    setState is working properly

--- src/Main.js
+++ src/Main.js
@@ -11,9 +11,9 @@ class Main extends React.Component {
         this.state = {
             displayInfo: false,
             searchQuery: '',
-            city: [],         
-            mapImg: '',
-            weatherData: []
+            city: [],
+            weatherData: [],
+            movieData: []
         }
     }
 
@@ -26,48 +26,31 @@ class Main extends React.Component {
     handleCitySearch = async (e) => {
         e.preventDefault();
         const url = `${process.env.REACT_APP_SERVER}/search?searchQuery=${this.state.searchQuery}`;
-        const response = await axios.get(url);
-        console.log(response.data[0]);
-        // this.setState({
-        //     city: response.data[0]}, 
-        //     () => console.log(this.state.city))
+        // const moviesUrl = `${process.env.REACT_APP_SERVER}/movies?searchQuery=${this.state.searchQuery}`;
+        const response = await axios.get(url)
+        this.setState({ city: response.data[0] }, () => console.log(this.state.city));
+        this.handleMovieSearch();
     }
-    
 
+    handleMovieSearch = async () => {
+        const url = `${process.env.REACT_APP_SERVER}/movies?searchQuery=${this.state.searchQuery}`;
+        const response = await axios.get(url);
+        this.setState({ movieData: response.data });
+        this.handleWeatherSearch();
+    }
 
-    // getMap = async (lat, lon) => {
-    //     const imageURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_COORDINATE_KEY}&center=${lat},${lon}&zoom=5`;
-    //     const response = await axios.get(imageURL);
-    //     this.setState({
-    //         mapImg: response.data.data
-    //     })
-    //     // console.log(this.state.mapImg)
-    // }
+    handleWeatherSearch = async () => {
+        const weatherUrl = `${process.env.REACT_APP_SERVER}/weather?lat=${this.state.city.lat}&lon=${this.state.city.lon}`;
+        const response = await axios.get(weatherUrl);
+        this.setState({ weatherData: response.data});
+    }
 
-    // submitHandler(e) {
-    //     this.getLocation(e);
-    //     // this.getMap();
-    //     // this.apiTest();
-    // }
-
-    // apiTest = async () => {
-    //     try {
-    //         // const url = `${process.env.REACT_APP_SERVER}/weather?searchQuery=${this.state.city}`;
-    //         const url = `${process.env.REACT_APP_SERVER}/weather?searchQuery=Seattle`;
-    //         const response = await axios.get(url);
-    //         this.setState({ weatherData: response.data },
-    //             () => console.log(this.state.weatherData))
-    //     }
-    //     catch (error) {
-    //         console.error(error.message);
-    //     }
-    // }
 
     render() {
+        console.log(this.state);
         return (
             <>
                 <Form onSubmit={this.handleCitySearch}>
-                {/* <Form onSubmit={this.apiTest}> */}
                     <Form.Group>
                         <Form.Label>Enter a name of a city</Form.Label>
                         <Form.Control type="text" onChange={this.handleInput}></Form.Control>
@@ -76,16 +59,12 @@ class Main extends React.Component {
                     {this.state.displayInfo &&
                         <>
                             <p>The city is {this.state.city.display_name}.</p>
-                            {/* <img src={this.state.city.icon}/> */}
                             <p>The latitude is {this.state.city.lat} and the longitude is {this.state.city.lon}.</p>
-                            {/* <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_COORDINATE_KEY}&center=${this.state.city.lat},${this.state.city.lon}&zoom=9`} alt="map"></img> */}
-
                         </>
 
                     }
-                    {/* </form> */}
                 </Form>
-                <Weather weatherData={this.state.weatherData}/>
+                <Weather weatherData={this.state.weatherData} />
             </>
         )
     }
